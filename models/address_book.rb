9223a7b04363e6718d843afceea6de073d5e177d
Jose Mena <josemena0114@gmail.com>
2016-08-05 12:23:03 +0000

    checkpoint-19-assignment-complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -17,7 +17,16 @@
        end
        index += 1
      end
-     # #11
      entries.insert(index, Entry.new(name, phone_number, email))
    end
+   
+   def remove_entry(name, phone_number, email)
+       entries.each do |entry|
+            if entry.name == name && entry.phone_number == phone_number && entry.email == email
+                entries.delete(entry)
+            end
+        end    
+   end
+   
+   
  end
