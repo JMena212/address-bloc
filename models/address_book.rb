1d415fbb1eda54d30b6af6179a01726ce5db7b83
Jose Mena <josemena0114@gmail.com>
2016-08-13 22:35:21 +0000

    checkpoint-22-assigmnment-complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -31,8 +31,18 @@
        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
      end
    end
-   
+    
+  
    # Search AddressBook for a specific entry by name
+   
+   def iterative_search(name)
+     entries.each do |entry|
+       if entry.name == name
+         return entry
+      end
+     end
+     return nil
+   end
    def binary_search(name)
  # #1
      lower = 0
